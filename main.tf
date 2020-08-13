87f9375a49468135525c473f68ccbeb61d19b09b
Chris Gilmer <chris.gilmer@gmail.com>
2020-08-13 11:57:50 -0700

    Pin tf version to 0.12 family. Update pre-commit.

--- main.tf
+++ main.tf
@@ -1,26 +1,3 @@
-/**
- * Enables AWS Config and configures any compliance changes or AWS Config service
- * changes to be sent to an SNS topic.
- *
- * Creates the following resources:
- *
- * * CloudWatch event rules to filter
- *   * AWS Config compliance changes
- *   * Changes to the AWS Config service
- * * CloudWatch event targets to send notifications to an SNS topic
- *
- * ## Usage
- *
- * ```hcl
- * module "config-notifications" {
- *   source = "trussworks/config-notifications/aws"
- *   version = "1.0.0"
- *
- *   sns_topic_name = "slack-events"
- * }
- * ```
- */
-
 #
 # SNS
 #
